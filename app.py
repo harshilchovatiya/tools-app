2a2a396d99b60bc6d7d39e61a999e6a12c86bb46
harshilchovatiya <131672641+harshilchovatiya@users.noreply.github.com>
2024-08-31 17:25:03 +0530

    up

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-from flask_cors import CORS
 from routes import main_routes_bp, qr_code_bp, url_shortener_bp, image_compressor_bp, image_upscaler_bp, file_converter_bp, image_to_text_bp, speedtest_bp, unit_converter_bp
 
 def create_app():
     app = Flask(__name__)
-    CORS(app) 
+    # CORS(app) 
     app.register_blueprint(main_routes_bp)
     app.register_blueprint(qr_code_bp)
     app.register_blueprint(url_shortener_bp)
